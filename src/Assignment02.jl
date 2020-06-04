18370a6a5da7585a927b15a42d78962b076fa8b6
Kevin Bonham <kevbonham@gmail.com>
2020-06-03 22:05:11 -0400

    still need tests for questions 3 and 4

--- src/Assignment02.jl
+++ src/Assignment02.jl
@@ -1,14 +1,13 @@
+# Do not modify this file
 module Assignment02
 
 export question1,
        question2,
        question3,
        question4,
-       question5
-
-# using BISC195
+       question5,
+       generate_sequence
 
 include("assignment.jl")
-# Write your package code here.
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,35 @@
 using Assignment02
 using Test
+using Random
+
+@testset "Assignment02" begin
+
+@testset "Setup" begin
+    @test isdefined(Assignment02, :generate_sequence)
+    Random.seed!(1)
+    s = generate_sequence(4)
+    @test length(s) == 4
+    @test s == "GGTC"
+end
 
 @testset "Question 1" begin
-    @test typeof(question1) <: AbstractString
+    @test question1 isa DataType
+    b = try
+        a = question1[]
+        push!(a, "test")
+    catch e
+        println(e)
+        a = []
+    end
+    @test length(b) == 1
 end
 
 @testset "Question 2" begin
-    @test typeof(question2) <: AbstractString
+    @test question2 isa Function
+    @test question2("string") isa Bool
+    @test question2("string")
+    @test question2(1.) isa Bool
+    @test !question2(1.)
 end
 
 @testset "Question 3" begin
@@ -20,3 +43,5 @@ end
 @testset "Question 5" begin
     @test typeof(question5) <: AbstractString
 end
+
+end # "Assignment02"
