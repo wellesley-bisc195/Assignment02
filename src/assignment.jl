b090781cace4f269380f45926d85ed5d4e22ca35
Kevin Bonham <kevbonham@gmail.com>
2021-06-03 15:22:24 -0400

    once more-3

--- src/assignment.jl
+++ src/assignment.jl
@@ -213,9 +213,9 @@ ce_2_2_2 = Bool
 
 ce_2_3_1 = Float64
 
-# > Suppose the cover price of a book is 24.95,
+# > Suppose the cover price of a book is \$24.95,
 # but bookstores get a 40 % discount.
-# Shipping costs \$ 3 for the first copy and 75 cents for each additional copy.
+# Shipping costs \$3 for the first copy and 75 cents for each additional copy.
 # What is the total wholesale cost for 60 copies?
 # 
 # Assign the variable to the correct answer.
